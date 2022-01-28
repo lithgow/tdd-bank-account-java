1f1266912880d1d196e643cc7a026108b719dfd7
Ben Lithgow Smith <benlithgow@gmail.com>
2022-01-28 13:02:56 +0000

    depositing money into an account

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,14 +2,21 @@ package org.xpdojo.bank;
 
 public class Account {
 
+    private int balance;
+
     public static Account emptyAccount() {
         return new Account();
     }
 
     private Account() {
+        balance = 0;
     }
 
     public int balance() {
-        return 0;
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance = amount;
     }
 }
